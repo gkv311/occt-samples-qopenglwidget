5c3a3f46e4155d4d7c751c3c7b91b0ead668a715
Kirill Gavrilov <kirill@sview.ru>
2025-12-17 21:16:44 +0300

    CI/CD - fix error in bash script screenshot.sh

--- .github/workflows/screenshot.sh
+++ .github/workflows/screenshot.sh
@@ -11,7 +11,7 @@ rm -f "$anImg"
 scrot -d $aDelay -F "$anImg"
 
 anImgSize=$(wc -c <"$anImg")
-if [ $actualsize -lt 2000 ]; then
+if [ $anImgSize -lt 2000 ]; then
   # retry one more time
   mv -f "$anImg" "back-first-${anImg}"
   scrot -d 1 -F "$anImg"
